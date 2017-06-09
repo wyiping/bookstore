0afb01f71e716ad5d6a0936cce10b8b3646b03f7
wangyiping <61780374@163.com>
2017-06-09 10:55:44 +0800

    feature:添加商品列表页面，完善首页显示

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,20 @@ const express = require("express");
 const db = require('../my_modules/db');
 
 var router = express.Router();
-
+function getPages(page, pageCount) {
+    var pages = [page];
+    var left = page - 1;
+    var right = page + 1;
+    // 左右两边各加1个页码,直到页码够10个或左边到1 右边到总页数
+    while (pages.length < 10 && (left >= 1 || right <= pageCount)) {
+        if (left > 0) pages.unshift(left--);
+        if (right <= pageCount) pages.push(right++);
+    }
+    return pages;
+}
 router.get('', (req, res) => {
     res.render('shop/home.html');
 })
-
 
 router.get('/login', (req, res) => {
     res.render('shop/login.html')
@@ -68,28 +77,41 @@ router.get('/logout', (req, res) => {
     res.render('shop/home.html');
 })
 
-// 查询书籍：page跳转的页面，：size每页显示数量
-router.get('/list/:page/:size', (req, res) => {
+// 查询书籍:view跳转的页面,:size每页显示数量,:page页数
+router.get('/list/:view/:size/(:page)?', (req, res) => {
+    var page = req.params.page;
+    page = page || 1;
+    page = parseInt(page);
+    var pageSize = parseInt(req.params.size);
+
     var filter = {};
-    var name = req.query.name;
+    var search = req.query.search;
     var type = req.query.type;
     var order = {};
     var o = req.query.order;
-    if (name) {
-        name = name.trim();
-        if (name.length > 0) {
-            filter.bookName = { '$regex': `.*${name}.*?` }
+    if (search) {
+        search = search.trim();
+        if (search.length > 0) {
+            filter.bookName = { '$regex': `.*${search}.*?` }
         }
     }
     if (type && type != 'undefined') {
         type = type.trim();
         filter.type = type;
     }
-    if(o && o!= undefined){
+    if (o && o != undefined) {
         order[o] = -1;
     }
-    db.Book.find(filter).sort(order).limit(parseInt(req.params.size)).exec((err, data) => {
-        res.render('shop/' + req.params.page + '.html', { books: data })
+    db.Book.find(filter).count((err, total) => {
+        var pageCount = Math.ceil(total / pageSize);
+        page = page > pageCount ? pageCount : page
+        page = page < 1 ? 1 : page;
+        db.Book.find(filter).sort(order).skip((page - 1) * pageSize).limit(pageSize).exec((err, data) => {
+            res.render('shop/' + req.params.view + '.html', {
+                page, pageCount, pageSize, order, pages: getPages(page, pageCount), type, search,
+                books: data
+            })
+        })
     })
 })
 
